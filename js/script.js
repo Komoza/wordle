c7f74796197d93e299fc058e6f0315c67f82f998
Maxim Komoza <93898606+Komoza@users.noreply.github.com>
2022-12-12 12:22:53 +1000

    game logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const original_word = 'Смола'.toUpperCase();
 let current_word = '';
+let curret_try = 1;
 
 function inpJump(el, course) {
     if (course === 'next' && el.value.length == 1 && document.activeElement.id != 'last_letter') {
@@ -31,7 +32,36 @@ function checkResult() {
         alert('Полностью введите слово');
         return;
     }
+    const items = document.querySelectorAll(`.try_${curret_try}`);
+    for (let i = 0; i < 5; i++) {
+        if (original_word[i] === current_word[i]) {
+            setColor(items[i], 'right letter')
+        }else if(original_word.indexOf(current_word[i]) != -1) {
+            setColor(items[i], 'wrong position');
+        }else {
+            setColor(items[i], 'wrong letter');
+        }
+    }
+    
     if (current_word === original_word) {
         alert('ПОБЕДА!');
+        return;
+    }
+    curret_try += 1;
+    document.activeElement.nextSibling.nextSibling.focus();
+    current_word = '';
+    
+}
+
+function setColor(element, key) {
+    if  (key === 'right letter') {
+        element.classList.add('right_letter');
+    } 
+    if (key === 'wrong position') {
+        element.classList.add('wrong_position');
+    }
+    if (key === 'wrong letter') {
+        element.classList.add('wrong_letter');
     }
 }
+
